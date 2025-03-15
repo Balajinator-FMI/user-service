bd919322d1bffcdaa5c8f522fd70e08a070fed48
Aleko Georgiev <algugone@gmail.com>
2025-03-16 01:37:58 +0200

    Minor change in enum

--- src/main/java/com/fmicodesproj/fmiprojuserservice/domain/SkinType.java
+++ src/main/java/com/fmicodesproj/fmiprojuserservice/domain/SkinType.java
@@ -2,9 +2,9 @@ package com.fmicodesproj.fmiprojuserservice.domain;
 
 public enum SkinType {
     PALE,
-    Fair,
-    Beige,
-    Olive,
-    Brown,
-    Dark,
+    FAIR,
+    BEIGE,
+    OLIVE,
+    BROWN,
+    DARK
 }
